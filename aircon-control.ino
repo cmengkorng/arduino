a4529bedaa7e5d7c38aa39771a5afcb7755d3395
cmengkorng <mengkorng.c@gmail.com>
2021-01-12 15:06:38 +0700

    use 2 output pole relay with neutral connect to aircon1

--- aircon-control.ino
+++ aircon-control.ino
@@ -1,20 +1,16 @@
-// set digital pin 2 for aircon1 and pin 3 for aircon2
-int aircon1 = 2;
-int aircon2 = 3;
+// set digital pin 2 for ctl to control the relay
+int ctl = 2;
 // set pin as output
 void setup() {
-  pinMode(aircon1,OUTPUT);
-  pinMode(aircon2,OUTPUT);
+  pinMode(ctl,OUTPUT);
 }
 // loop
-// HIGH is assign 5V to OUTPUT pin
-// LOW is assign 0V to OUTPUT pin
-// delay(5*60*60*1000) is equal to 5h delay
 void loop() {
-  digitalWrite(aircon1, HIGH);
-  digitalWrite(aircon2, LOW);
+  digitalWrite(ctl, LOW);
+  // provide 0V to relay so ac source connect to aircon1
   delay(5*60*60*1000);
-  digitalWrite(aircon1, LOW);
-  digitalWrite(aircon2, HIGH);
+  // delay for 5h
+  digitalWrite(ctl, HIGH);
+  // provide 5V to relay so ac source connect to aircon2
   delay(5*60*60*1000);
 }
